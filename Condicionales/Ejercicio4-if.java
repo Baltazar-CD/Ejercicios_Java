bb22e9d9511bde0e86e5d0970c1a8152edf4d554
Campos Delgado Baltazar <91445820+Baltazar-CD@users.noreply.github.com>
2021-09-28 23:35:53 -0600

    Add files via upload

---
+++ Condicionales/Ejercicio4-if.java
@@ -0,0 +1,27 @@
+/* Instituto Tecnológico de Culiacán
+Campos Delgado Baltazar
+Unidad 2 Fundamentos de programacion */
+
+import java.util.Scanner;
+
+public class Ejercicio {
+
+    public static void main(String[] args) { 
+        Scanner teclado = new Scanner(System.in);
+        int sueldo;
+        System.out.print("[*] Ingrese el sueldo del empledo: ");
+        sueldo = teclado.nextInt();
+        System.out.print("[*] Ingrese los años del empleado en la compañia: ");
+        int años = teclado.nextByte();
+        
+        if (años >= 5){
+            System.out.println("[+] Recibira un aumento en su sueldo del 18% por estar tanto tiempo en la compañia");
+            float porcen = 0.18f * sueldo;
+            float aumento = porcen+sueldo;
+            System.out.println("[+] Su nuevo sueldo es de: " + aumento);
+        }else{
+            System.out.println("[+] Su sueldo por el momento seguira siendo el mismo, de: " + sueldo);
+        }
+    }
+    
+}
